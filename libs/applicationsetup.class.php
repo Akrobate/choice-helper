e541e0f3330908b80308dc71a9bfbe8287160322
ArtiNomi <afedorov@nomination.fr>
2017-03-03 13:49:48 +0100

    Structure table creation step, adding users table

--- libs/applicationsetup.class.php
+++ libs/applicationsetup.class.php
@@ -1,58 +1,57 @@
 <?php
 
+/**
+ *	Class used by script installation to prepare data storage
+ *	Creates tables
+ */
+
 class ApplicationSetup {
 
-	private static $params;
-
-
-	public static function createItemsTable() {
-		/*
-		CREATE TABLE `items` (
-		  `id` bigint(24) unsigned NOT NULL AUTO_INCREMENT,
-		  `title` varchar(255) NOT NULL,
-		  `value` text NOT NULL,
-		  `created` datetime NOT NULL,
-		  `updated` datetime NOT NULL,
-		  `rank` float(10,5) NOT NULL,
-		  PRIMARY KEY (`id`)
-		) ENGINE=MyISAM AUTO_INCREMENT=0 DEFAULT CHARSET=latin1;
-		*/
+	public static function queryCreateItemsTable() {
+		$query = '
+			CREATE TABLE `items` (
+			  `id` bigint(24) unsigned NOT NULL AUTO_INCREMENT,
+			  `title` varchar(255) NOT NULL,
+			  `value` text NOT NULL,
+			  `created` datetime NOT NULL,
+			  `updated` datetime NOT NULL,
+			  `rank` float(10,5) NOT NULL,
+			  PRIMARY KEY (`id`)
+			) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=UTF8;
+		';
+		return $query;
     }
 
-    public static function createCollectionsTable() {
 
-/*
-
-    CREATE TABLE IF NOT EXISTS `collections` (
-      `id` int(11) NOT NULL,
-      `label` varchar(255) NOT NULL,
-      `user_id` int(11) NOT NULL,
-      `created` datetime NOT NULL,
-      `updated` datetime NOT NULL
-    ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-    --
-    -- Indexes for dumped tables
-    --
-
-    --
-    -- Indexes for table `collections`
-    --
-    ALTER TABLE `collections`
-      ADD PRIMARY KEY (`id`);
-
-    --
-    -- AUTO_INCREMENT for dumped tables
-    --
-
-    --
-    -- AUTO_INCREMENT for table `collections`
-    --
-    ALTER TABLE `collections`
-      MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-*/
-
+    public static function queryCreateCollectionsTable() {
+		$query = '
+		    CREATE TABLE IF NOT EXISTS `collections` (
+		      `id` bigint(24) NOT NULL AUTO_INCREMENT,
+		      `label` varchar(255) NOT NULL,
+		      `user_id` int(11) NOT NULL,
+		      `created` datetime NOT NULL,
+		      `updated` datetime NOT NULL,
+			  PRIMARY KEY (`id`)
+		    ) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=UTF8;
+		';
+		return $query;
     }
 
+
+    public static function queryCreateUsersTable() {
+		$query = '
+		    CREATE TABLE IF NOT EXISTS `users` (
+		      `id` bigint(24) NOT NULL AUTO_INCREMENT,
+		      `email` varchar(255) NOT NULL,
+		      `first_name` int(11) NOT NULL,
+		      `last_name` datetime NOT NULL,
+			  `token` TEXT NOT NULL,
+			  `created` datetime NOT NULL,
+		      `updated` datetime NOT NULL,
+			  PRIMARY KEY (`id`)
+		    ) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=UTF8;
+		';
+		return $query;
+    }
 
 }
